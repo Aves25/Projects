6d866de993fcb434c95360219219d5c948dd8aa4
Aves25 <143750502+Aves25@users.noreply.github.com>
2023-09-07 11:53:52 -0400

    Add files via upload

---
+++ Putouts From Players That Would be Considered Below Average - Average,  2015 and Later.sql
@@ -0,0 +1,22 @@
+SELECT
+p.nameFirst,
+p.nameLast,
+f.playerID,
+f.yearID,
+f.PO,
+CASE
+WHEN f.PO < 500 THEN "Bad"
+WHEN f.PO BETWEEN 500 AND 1000 THEN "Below Average"
+WHEN f.PO BETWEEN 1000 AND 1499 THEN "Average"
+WHEN f.PO >=1500 THEN "Good"
+END AS Productivity
+FROM
+Fielding AS f
+INNER JOIN
+People AS p
+ON
+f.playerID = p.playerID
+WHERE
+Productivity IN ("Below Average", "Average") AND yearID>= 2015
+ORDER BY
+f.PO DESC
